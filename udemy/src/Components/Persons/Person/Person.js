bfd24a2d183111371629788a651afd73e21048d8
jkaddu <jkaddu@yahoo.com>
2020-02-13 20:33:50 -0800

    refactored peron.js into a class compnonet

--- udemy/src/Components/Persons/Person/Person.js
+++ udemy/src/Components/Persons/Person/Person.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Person.module.css";
 
-const person = props => {
-  console.log("[Person.js] rendering...");
-  return (
-    <div className={classes.UserOutput}>
-      <p onClick={props.click}>Username: {props.name}</p>
-      <p>Age: {props.age}</p>
-      <input
-        type="text"
-        placeholder="username"
-        onChange={props.changed}
-        value={props.name}
-      />
-    </div>
-  );
-};
+class Person extends Component {
+  render() {
+    console.log("[Person.js] rendering...");
+    return (
+      <div className={classes.UserOutput}>
+        <p onClick={this.props.click}>Username: {this.props.name}</p>
+        <p>Age: {this.props.age}</p>
+        <input
+          type="text"
+          placeholder="username"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
